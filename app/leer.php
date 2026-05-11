39e1f393fee2e25886130e4df276615feaf13036
IRios79 <1979irr@gmail.com>
2026-05-11 22:17:18 +0200

    Visualización en navegador y descarga de archivos

--- app/leer.php
+++ app/leer.php
@@ -123,16 +123,22 @@ if (isset($_POST['nueva_carpeta'])) {
         foreach ($files as $f) {
             $full = $current_path . '/' . $f;
             $isDir = is_dir($full);
-            // Enlace para abrir carpeta o #
-            $link = $isDir ? "leer.php?dir=" . urlencode(($req ? $req.'/' : '') . $f) : "#";
+
+            // Si es carpeta, recarga leer.php. Si es archivo, lo envía al portero.
+            $link = $isDir ? "leer.php?dir=" . urlencode(($req ? $req.'/' : '') . $f) : "abrir_archivos.php?file=" . urlencode($f) . "&dir=" . urlencode($req);
+            
+            // Si es un archivo, queremos que intente abrirse en una pestaña nueva de tu navegador (_blank)
+            $target = $isDir ? "" : " target='_blank'";
+            
             $icon = $isDir ? "📁" : "📄";
+
             $size = $isDir ? "-" : round(filesize($full)/1024, 2) . " KB";
 
             echo "<tr>";
             
             // Columna Nombre + Renombrar
             echo "<td>
-                    <a href='$link' style='font-weight:bold; font-size:1.1em;'>$icon $f</a>
+                    <a href='$link'$target style='font-weight:bold; font-size:1.1em; color: #1f2937; text-decoration: none;'>$icon $f</a>
                     <form method='post' style='display:inline-block; margin-left:15px; opacity:0.6;'>
                         <input type='hidden' name='archivo_original' value='$f'>
                         <input type='text' name='nuevo_nombre' placeholder='Renombrar' style='padding:2px; font-size:12px; width:80px; margin:0;'>
